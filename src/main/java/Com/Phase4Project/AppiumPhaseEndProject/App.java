0b8429322e280afad19417a4cb7325bddc62b422
Your Name <you@example.com>
2023-12-15 14:37:03 +0530

    Save file

---
+++ src/main/java/Com/Phase4Project/AppiumPhaseEndProject/App.java
@@ -0,0 +1,13 @@
+package Com.Phase4Project.AppiumPhaseEndProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
